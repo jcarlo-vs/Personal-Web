8f36cd143203e06f6f667053a24f6a58e7778998
Juan <juan.dev.ph>
2023-03-30 16:55:37 +0800

    fix possible text on mobile

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { TextProps } from './types'
 
 const Text = ({ size, className, children }: TextProps) => {
-  return <p className={cx(`text-${size}`, className)}>{children}</p>
+  return <p className={cx(`text-${size} text-white`, className)}>{children}</p>
 }
 
 export default Text
