e0991dbe3039488f1734ac4534798f48334a6fa1
jcvs-moorup <jc.senin@moorup.com.au>
2025-02-26 22:56:47 +0800

    update

--- src/views/Sections/Resume.tsx
+++ src/views/Sections/Resume.tsx
@@ -1,127 +1,127 @@
-'use client'
+"use client";
 /* eslint-disable react/no-unescaped-entities */
-import { Section } from '@/components/Section'
-import { Text } from '@/components/Text'
-import cx from 'clsx'
-import { BsCassette } from 'react-icons/bs'
-import { useInView } from 'react-intersection-observer'
+import { Section } from "@/components/Section";
+import { Text } from "@/components/Text";
+import cx from "clsx";
+import { BsCassette } from "react-icons/bs";
+import { useInView } from "react-intersection-observer";
 
 const Resume = () => {
   const { ref, inView, entry } = useInView({
     /* Optional options */
     threshold: 0.1,
-    rootMargin: '-100px',
+    rootMargin: "-100px",
     triggerOnce: true,
-  })
+  });
 
   return (
-    <Section id='Resume' icon={<BsCassette />} name='Resume'>
+    <Section id="Resume" icon={<BsCassette />} name="Resume">
       <div
         ref={ref}
         className={cx(
-          'transition delay-300',
-          inView ? 'blur-0' : 'blur-md transition delay-300'
+          "transition delay-300",
+          inView ? "blur-0" : "blur-md transition delay-300"
         )}
       >
         <div>
           <Text
-            size='head'
-            className='font-mono  max-[1024px]:!text-3xl max-[768px]:!text-2xl'
+            size="head"
+            className="font-mono  max-[1024px]:!text-3xl max-[768px]:!text-2xl"
           >
             Education &
             <span
               style={{
-                background: 'linear-gradient(90deg,#00dbde,#fc00ff)',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-                textShadow: '#9c0a88 1px 20px 100px',
+                background: "linear-gradient(90deg,#00dbde,#fc00ff)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                textShadow: "#9c0a88 1px 20px 100px",
               }}
-              className='ml-5'
+              className="ml-5"
             >
               Experience
             </span>
           </Text>
         </div>
 
-        <div className='flex flex-col gap-16 mt-9'>
-          <div className='flex flex-col gap-2'>
+        <div className="flex flex-col gap-16 mt-9">
+          <div className="flex flex-col gap-2">
             <Text
-              size='subtitle'
-              className='max-w-2xl font-light font-sans !text-pink-500 text-md'
+              size="subtitle"
+              className="max-w-2xl font-light font-sans !text-pink-500 text-md"
             >
-              DECEMBER 2022 - PRESENT
+              DECEMBER 2022 - JUNE 2023
             </Text>
             <Text
-              size='title'
-              className='max-w-2xl font-light font-sans text-slate-100 text-3xl max-[1024px]:!text-2xl max-[768px]:!text-xl'
+              size="title"
+              className="max-w-2xl font-light font-sans text-slate-100 text-3xl max-[1024px]:!text-2xl max-[768px]:!text-xl"
             >
-              FRONT END DEVELOPER
+              Software Engineer
             </Text>
             <Text
-              size='normal'
-              className='max-w-2xl font-light font-sans text-slate-400 text-md'
+              size="normal"
+              className="max-w-2xl font-light font-sans text-slate-400 text-md"
             >
               Vtime Tech Consulting Inc.
             </Text>
           </div>
 
-          <div className='flex flex-col gap-2'>
+          <div className="flex flex-col gap-2">
             <Text
-              size='subtitle'
-              className='max-w-2xl font-light font-sans !text-pink-500 text-md'
+              size="subtitle"
+              className="max-w-2xl font-light font-sans !text-pink-500 text-md"
             >
               JANUARY 2022 - DECEMBER 2022
             </Text>
             <Text
-              size='title'
-              className='max-w-2xl font-light font-sans text-slate-100 text-3xl max-[1024px]:!text-2xl max-[768px]:!text-xl'
+              size="title"
+              className="max-w-2xl font-light font-sans text-slate-100 text-3xl max-[1024px]:!text-2xl max-[768px]:!text-xl"
             >
-              FRONT END DEVELOPER
+              Software Engineer
             </Text>
             <Text
-              size='normal'
-              className='max-w-2xl font-light font-sans text-slate-400 text-md'
+              size="normal"
+              className="max-w-2xl font-light font-sans text-slate-400 text-md"
             >
               Lobster Capital
             </Text>
           </div>
 
-          <div className='flex flex-col gap-2'>
+          <div className="flex flex-col gap-2">
             <Text
-              size='subtitle'
-              className='max-w-2xl font-light font-sans !text-pink-500 text-md'
+              size="subtitle"
+              className="max-w-2xl font-light font-sans !text-pink-500 text-md"
             >
               JULY 2017 - APRIL 2021
             </Text>
             <Text
-              size='title'
-              className='max-w-2xl font-light font-sans text-slate-100 text-3xl max-[1024px]:!text-2xl max-[768px]:!text-xl'
+              size="title"
+              className="max-w-2xl font-light font-sans text-slate-100 text-3xl max-[1024px]:!text-2xl max-[768px]:!text-xl"
             >
-              Chart Analyst / Strategic Advisor
+              Research Analyst
             </Text>
             <Text
-              size='normal'
-              className='max-w-2xl font-light font-sans text-slate-400 text-md'
+              size="normal"
+              className="max-w-2xl font-light font-sans text-slate-400 text-md"
             >
               Seal Capital - London, England
             </Text>
           </div>
-          <div className='flex flex-col gap-2'>
+          <div className="flex flex-col gap-2">
             <Text
-              size='subtitle'
-              className='max-w-2xl font-light font-sans !text-pink-500 text-md'
+              size="subtitle"
+              className="max-w-2xl font-light font-sans !text-pink-500 text-md"
             >
               2013 - 2018
             </Text>
             <Text
-              size='title'
-              className='max-w-2xl font-light font-sans text-slate-100 max-[1024px]:!text-2xl max-[768px]:!text-xl'
+              size="title"
+              className="max-w-2xl font-light font-sans text-slate-100 max-[1024px]:!text-2xl max-[768px]:!text-xl"
             >
               Bachelor Degree of Computer Engineering
             </Text>
             <Text
-              size='normal'
-              className='max-w-2xl font-light font-sans text-slate-400 text-md'
+              size="normal"
+              className="max-w-2xl font-light font-sans text-slate-400 text-md"
             >
               University of Pangasinan
             </Text>
@@ -129,7 +129,7 @@ const Resume = () => {
         </div>
       </div>
     </Section>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
--- src/views/Sections/Skills.tsx
+++ src/views/Sections/Skills.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 /* eslint-disable react/no-unescaped-entities */
-import { Text } from '@/components/Text'
-import { BsPerson } from 'react-icons/bs'
+import { Text } from "@/components/Text";
+import { BsPerson } from "react-icons/bs";
 import {
   css,
   express,
@@ -20,11 +20,11 @@ import {
   styledComponents,
   tailwind,
   ts,
-} from '@/assets/icons'
-import TechIcon from '@/components/TechIcon/TechIcon'
-import { Section } from '@/components/Section'
-import { useInView } from 'react-intersection-observer'
-import cx from 'clsx'
+} from "@/assets/icons";
+import TechIcon from "@/components/TechIcon/TechIcon";
+import { Section } from "@/components/Section";
+import { useInView } from "react-intersection-observer";
+import cx from "clsx";
 
 const Skills = () => {
   const { ref, inView, entry } = useInView({
@@ -32,35 +32,35 @@ const Skills = () => {
     threshold: 0.3,
 
     triggerOnce: true,
-  })
+  });
 
   return (
     <div>
       <Section
-        id='Skills'
-        className='relative'
+        id="Skills"
+        className="relative"
         icon={<BsPerson />}
-        name='Skills'
+        name="Skills"
       >
         <div
           ref={ref}
           className={cx(
-            'transition duration-500 ease-in',
-            inView ? '-translate-x-0 blur-0' : '-translate-x-[60%] blur-lg'
+            "transition duration-500 ease-in",
+            inView ? "-translate-x-0 blur-0" : "-translate-x-[60%] blur-lg"
           )}
         >
           <Text
-            size='head'
-            className='font-mono text-4xl max-[1024px]:!text-3xl max-[768px]:!text-2xl'
+            size="head"
+            className="font-mono text-4xl max-[1024px]:!text-3xl max-[768px]:!text-2xl"
           >
             <span
               style={{
-                background: 'linear-gradient(90deg,#00dbde,#fc00ff)',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-                textShadow: '#9c0a88 1px 20px 100px',
+                background: "linear-gradient(90deg,#00dbde,#fc00ff)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                textShadow: "#9c0a88 1px 20px 100px",
               }}
-              className='mr-3'
+              className="mr-3"
             >
               Technologies
             </span>
@@ -69,19 +69,19 @@ const Skills = () => {
         </div>
 
         {/* FRONTEND */}
-        <section className=' mt-10'>
-          <div className='mb-3'>
+        <section className=" mt-10">
+          <div className="mb-3">
             <span
               style={{
-                background: 'linear-gradient(90deg,#00dbde,#fc00ff)',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-                textShadow: '#9c0a88 1px 20px 100px',
+                background: "linear-gradient(90deg,#00dbde,#fc00ff)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                textShadow: "#9c0a88 1px 20px 100px",
               }}
               ref={ref}
               className={cx(
-                'py-9 tracking-widest transition duration-700 ease-in',
-                inView ? '-translate-x-0 blur-0' : '-translate-x-[60%] blur-lg'
+                "py-9 tracking-widest transition duration-700 ease-in",
+                inView ? "-translate-x-0 blur-0" : "-translate-x-[60%] blur-lg"
               )}
             >
               FRONT-END
@@ -93,39 +93,39 @@ const Skills = () => {
           <div
             ref={ref}
             className={cx(
-              'flex gap-3 flex-wrap max-w-md transition duration-700 ease-in',
-              inView ? '-translate-x-0 blur-0' : '-translate-x-[60%] blur-sm'
+              "flex gap-3 flex-wrap max-w-md transition duration-700 ease-in",
+              inView ? "-translate-x-0 blur-0" : "-translate-x-[60%] blur-sm"
             )}
           >
-            <TechIcon image={html} title='HTML' />
-            <TechIcon image={css} title='CSS' />
-            <TechIcon image={tailwind} title='TAILWIND' />
-            <TechIcon image={bootstrap} title='BOOTSTRAP' />
-            <TechIcon image={framer} title='FRAMER' />
+            <TechIcon image={html} title="HTML" />
+            <TechIcon image={css} title="CSS" />
+            <TechIcon image={tailwind} title="TAILWIND" />
+            <TechIcon image={bootstrap} title="BOOTSTRAP" />
+            <TechIcon image={framer} title="FRAMER" />
 
-            <TechIcon image={styledComponents} title='STYLED COMPONENTS' />
+            <TechIcon image={styledComponents} title="STYLED COMPONENTS" />
 
-            <TechIcon image={js} title='JAVASCRIPT' />
-            <TechIcon image={ts} title='TYPESCRIPT' />
-            <TechIcon image={reactjs} title='REACT JS' />
-            <TechIcon image={nextjs} title='NEXT JS' />
+            <TechIcon image={js} title="JAVASCRIPT" />
+            <TechIcon image={ts} title="TYPESCRIPT" />
+            <TechIcon image={reactjs} title="REACT JS" />
+            <TechIcon image={nextjs} title="NEXT JS" />
           </div>
         </section>
 
         {/* BACKEND */}
-        <section className=' mt-10'>
-          <div className='mb-3'>
+        <section className=" mt-10">
+          <div className="mb-3">
             <span
               style={{
-                background: 'linear-gradient(90deg,#00dbde,#fc00ff)',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-                textShadow: '#9c0a88 1px 20px 100px',
+                background: "linear-gradient(90deg,#00dbde,#fc00ff)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                textShadow: "#9c0a88 1px 20px 100px",
               }}
               ref={ref}
               className={cx(
-                'py-9 tracking-widest transition duration-700 delay-200 ease-in',
-                inView ? '-translate-x-0 blur-0' : '-translate-x-[60%] blur-lg'
+                "py-9 tracking-widest transition duration-700 delay-200 ease-in",
+                inView ? "-translate-x-0 blur-0" : "-translate-x-[60%] blur-lg"
               )}
             >
               BACK-END
@@ -137,31 +137,31 @@ const Skills = () => {
           <div
             ref={ref}
             className={cx(
-              'flex gap-3 flex-wrap max-w-md transition duration-700 delay-200 ease-in',
-              inView ? '-translate-x-0 blur-0' : '-translate-x-[60%] blur-sm'
+              "flex gap-3 flex-wrap max-w-md transition duration-700 delay-200 ease-in",
+              inView ? "-translate-x-0 blur-0" : "-translate-x-[60%] blur-sm"
             )}
           >
-            <TechIcon image={mongoDB} title='MONGO DB' />
-            <TechIcon image={express} title='EXPRESS JS' />
-            <TechIcon image={nodejs} title='NODE JS' />
+            <TechIcon image={mongoDB} title="MONGO DB" />
+            <TechIcon image={express} title="EXPRESS JS" />
+            <TechIcon image={nodejs} title="NODE JS" />
           </div>
         </section>
 
         {/* OTHER TOOLS */}
 
-        <section className=' mt-4'>
-          <div className='mb-3'>
+        <section className=" mt-4">
+          <div className="mb-3">
             <span
               style={{
-                background: 'linear-gradient(90deg,#00dbde,#fc00ff)',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-                textShadow: '#9c0a88 1px 20px 100px',
+                background: "linear-gradient(90deg,#00dbde,#fc00ff)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                textShadow: "#9c0a88 1px 20px 100px",
               }}
               ref={ref}
               className={cx(
-                'py-9 tracking-widest max-w-md transition duration-700 delay-300 ease-in',
-                inView ? '-translate-x-0 blur-0' : '-translate-x-[60%] blur-lg'
+                "py-9 tracking-widest max-w-md transition duration-700 delay-300 ease-in",
+                inView ? "-translate-x-0 blur-0" : "-translate-x-[60%] blur-lg"
               )}
             >
               OTHER TOOLS
@@ -173,18 +173,18 @@ const Skills = () => {
           <div
             ref={ref}
             className={cx(
-              'flex gap-3 flex-wrap max-w-md transition duration-700 delay-300 ease-in',
-              inView ? '-translate-x-0 blur-0' : '-translate-x-[60%] blur-sm'
+              "flex gap-3 flex-wrap max-w-md transition duration-700 delay-300 ease-in",
+              inView ? "-translate-x-0 blur-0" : "-translate-x-[60%] blur-sm"
             )}
           >
-            <TechIcon image={git} title='GIT' />
+            <TechIcon image={git} title="GIT" />
 
-            <TechIcon image={ps} title='PHOTOSHOP' />
+            <TechIcon image={ps} title="PHOTOSHOP" />
           </div>
         </section>
       </Section>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
